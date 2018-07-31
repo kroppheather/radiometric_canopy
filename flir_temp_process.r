d3be4ad0ed23da13b3b1c620f399e60151eeeaef
kroppheather <kroppheather@gmail.com>
2018-07-31 16:19:43 -0400

    finished code for generating jpeg from flir ir output that has consistent color across temperature

--- flir_temp_process.r
+++ flir_temp_process.r
@@ -72,7 +72,59 @@ for(i in 1:length(flightDIR)){
 }
 
 #read in csvs and set up tiff for each one
+flightDF <- list()
+lyseq <- seq(0,Ncol-1)
 #files are 120x 160 pixels
+for(i in 1:length(flightDIR)){
+	for(j in 1:length(flightFiles[[i]])){
+		#read in data file and reorganize
+		flightDataR[[j]] <- read.csv(paste0(imgDIR,"\\",flightDIR[i],"\\",flightFiles[[i]][j]),head=FALSE)
+		
+		flightDF[[j]] <- data.frame(Y=rep(seq(1,120),each=160),X=rep(seq(1,160),times=120),
+								Temp=as.vector(data.matrix(flightDataR[[j]])))
+		#assign a color for each pixel
+		crange <- flightQ[[i]]
+		ccol <- rainbow(Ncol)
+		pcol <- character(0)
+		for(k in 1:dim(flightDF[[j]])[1]){
+			for(m in 1:(Ncol-1)){
+				if(flightDF[[j]]$Temp[k]>=crange[m]&flightDF[[j]]$Temp[k]<crange[m+1]){
+					pcol[k]<-ccol[m]
+				}
+			}	
+	
+		}
+		flightDF[[j]]$pcol <- pcol
+	
+		jpeg(paste0(outDIR,"\\",flightDIR[i],"\\",gsub(".csv","",flightFiles[[i]][j]),".jpg"),
+			width=640,height=480, units="px")
+			
+		par(mai=c(0,0,0,0))	
+		plot(c(0,1),c(0,1), type="n", ylim=c(120,0),xlim=c(0,160), xaxs="i",yaxs="i",
+			xlab=" ", ylab=" ")
+			
+			for(k in 1:dim(flightDF[[j]])[1]){
+				polygon(c(flightDF[[j]]$X[k]-1,flightDF[[j]]$X[k]-1,flightDF[[j]]$X[k],flightDF[[j]]$X[k]),
+						c(flightDF[[j]]$Y[k]-1,flightDF[[j]]$Y[k],flightDF[[j]]$Y[k],flightDF[[j]]$Y[k]-1),
+						border=NA, col=flightDF[[j]]$pcol[k])
+			}			
+			
+		dev.off()	
+	}
+	#make a legend for the flight color and temp
+	jpeg(paste0(outDIR,"\\",flightDIR[i],"\\temperature_legend.jpg"),width=500,height=1000, units="px")
+		layout(matrix(c(1),ncol=1), width=c(lcm(5)), height=c(lcm(30)))	
+	
+		plot(c(0,1),c(0,1),  type="n", ylim=c(0,Ncol),xlim=c(0,1), xaxs="i",yaxs="i",
+			xlab=" ", ylab=" ",axes=FALSE)
+			for(k in 1:(Ncol-1)){
+				polygon(c(0,0,1,1), c(lyseq[k],lyseq[k]+1,lyseq[k]+1,lyseq[k]),col=ccol[k], border=NA)
+			}
+		
+		axis(4,lyseq,round(flightQ[[i]],2),las=2,cex.axis=2)
+	dev.off()
+}	
+
 
 ############################################
 ### get photo times from rgb             ###
